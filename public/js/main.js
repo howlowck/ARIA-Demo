2c8e35bcf0c4924be98f63d5e2f3881060545cf0
Hao Luo <howlowck@gmail.com>
2012-11-30 16:53:46 -0600

    added best practices, added lang

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,8 @@ $(function() {
 		html = $(this).html();
 		html = html.replace(/</g,"&lt;");
 		html = html.replace(/>/g,"&gt;");
+		html = html.replace(/\\t/g,"&nbsp;&nbsp;&nbsp;&nbsp;");
+		html = html.replace(/\\n/g,"<br>");
 		$(this).html(html);
 	});
 
@@ -11,5 +13,6 @@ $(function() {
 
 	$(".remove-code").click(function(){
 		$(".code-example").remove();
+		$(this).remove();
 	});
  });
